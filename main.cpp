aefbc7afe2c9d6d0bbc3581a81c93231d3f30dd8
MShaza <mshazaib3@outlook.com>
2025-01-25 12:28:16 +0100

    more functionalities change, create and list function added

--- main.cpp
+++ main.cpp
@@ -14,12 +14,11 @@ public:
     Node(string fileName, bool isFile): fileName(fileName), isFile(isFile){}
    
 };
-class VFS
+class VirtualFileSystem
 {
 private:
    Node * rootNode;
    Node * currentDirectory;
-   VFS(Node* rootNode, Node* curretDirectory): rootNode(new Node("/",false)),currentDirectory(rootNode){}
    Node *checkIfExist(Node* directory, string fileName){
     for(auto directorychild : directory->directoryChildrens){
         if(fileName == directorychild->fileName){
@@ -30,22 +29,93 @@ private:
     
    }
 public:
+VirtualFileSystem(): rootNode(new Node("/",false)),currentDirectory(rootNode){}
+
 void createDirectory(string fileName, bool isFile);
+void listDirectory();
+void changeDirectory(string directoryName);
+void commandsHelp();
+void getCommands();
 };
 /**
  * @func : Create Directory
  * @parameters : Current Direcorty
 */
-void::VFS::createDirectory(string fileName, bool isFile){
+void::VirtualFileSystem::createDirectory(string fileName, bool isFile){
     //check if file already exist
       if(checkIfExist(currentDirectory, fileName)){
         cout<<"File already exist"<<endl;
+        return;
     }
     Node* newNode = new Node(fileName, isFile);
         currentDirectory->directoryChildrens.push_back(newNode);
         std::cout << (isFile ? "File" : "Directory") << " created: " << fileName << "\n";
+        return;
     }
-int main(){
-
+void::VirtualFileSystem::listDirectory(){
+    if(currentDirectory->directoryChildrens.empty()){
+        cout<<"There are no files in the driectory "<<currentDirectory->fileName<<endl;
+        return;
+    }
+    for(auto directoryChildrens:currentDirectory->directoryChildrens){
+        cout<<directoryChildrens->fileName<<std::endl;
+    }
+    return;
 }
+void::VirtualFileSystem::changeDirectory(string changeDirectory){
+    if(changeDirectory == ".."){
+        currentDirectory = rootNode;
+    }
+    else{
+        Node* targetDirectory = checkIfExist(currentDirectory, changeDirectory);
+        if (targetDirectory != nullptr) {
+            // Change current directory to the target directory
+            currentDirectory = targetDirectory;
+        } else {
+            // Directory does not exist
+            cout << "Directory does not exist" << endl;
+        }
+    }
+}
+void::VirtualFileSystem::commandsHelp(){
+    cout<<"cd <name>        :                  Change Directory"<<endl;
+    cout<<"mkdir <name>     :                  Create Diretory"<<endl;
+    cout<<"touch <name>     :                  Create file"<<endl;
+    cout<<"ls               :                  List all the files in a direcroty"<<endl;
+    cout<<"rm <name>        :                  Remove directory/file"<<endl;
+}
+void::VirtualFileSystem::getCommands(){
+    string getCommand;
+    string fileName;
+    cout<<currentDirectory->fileName<<": ";
+     while (getline(cin, getCommand)) {
 
+        if (getCommand == "exit") break; // Exit the loop
+        else if(getCommand == "ls"){
+            listDirectory();
+        }
+        else if (getCommand.substr(0, 5) =="mkdir")
+        {
+            fileName = getCommand.substr(6,getCommand.length());
+            
+            createDirectory(fileName, false);
+        }
+        else if(getCommand.substr(0, 5) =="touch"){
+            fileName = getCommand.substr(6,getCommand.length());
+            createDirectory(fileName, true);
+        }
+        
+        else if (getCommand.substr(0,2)=="cd"){
+            fileName = getCommand.substr(3,getCommand.length());
+            changeDirectory(fileName);
+        }
+        else if(getCommand == "help"){
+            commandsHelp();
+        }
+         cout<<currentDirectory->fileName<<": ";
+     }
+}
+int main(){
+    VirtualFileSystem vfs;
+    vfs.getCommands();
+}
